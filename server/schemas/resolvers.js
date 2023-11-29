28a6ebdb2af338865cf541b2ab7b43af77a4306f
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-11-28 19:31:33 -0500

    converted controler code into resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,22 +1,76 @@
-const { Tech, Matchup } = require('../models');
-const { GraphQLError } = require("graphql")
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    getSingleUser: async (parent, { userId, username }) => {
+      const foundUser = await User.findOne({
+        $or: [{ _id: userId }, { username }],
+      });
+
+      if (!foundUser) {
+        throw AuthenticationError;
+      }
+
+      return foundUser;
     },
   },
+
   Mutation: {
-    createUser: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
-    login: async (parent, { email, password }) => {
-      
+
+    login: async (parent, { username, email, password }) => {
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, { userId, book }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: userId },
+        { $addToSet: { savedBooks: book } },
+        { new: true, runValidators: true }
+      );
+
+      if (!updatedUser) {
+        throw AuthenticationError;
+      }
+
+      return updatedUser;
+    },
+
+    deleteBook: async (parent, { userId, bookId }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        throw AuthenticationError;
+      }
+
+      return updatedUser;
     },
   },
 };
