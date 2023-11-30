b7dc4432010a2f4e025f83e92352be8fd89526d0
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-11-29 22:40:37 -0500

    fixed import statement

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,6 +9,7 @@ import {
   Row
 } from 'react-bootstrap';
 import { SEARCH_GOOGLE_BOOKS, SAVE_BOOK } from '../utils/queries';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { getSavedBookIds, saveBookIds } from '../utils/localStorage';
 
