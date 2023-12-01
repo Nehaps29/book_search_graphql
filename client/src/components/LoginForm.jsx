6860720e24c7b2017fb2312d2e296d96f831f323
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-12-01 17:48:21 -0500

    fixed import in login page

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,13 +3,16 @@ import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 //import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
 
-const LoginForm = () => {
+
+const LoginForm = (props) => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
+  const [login, { error, data }] = useMutation(LOGIN_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -26,15 +29,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      //const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData }
+      });
+      
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
